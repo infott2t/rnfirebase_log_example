25ae190978f4daf06d6bfed2dbde7992e4029911
Hyeonil Choi <tt2t2am1118@naver.com>
2020-03-24 03:24:27 +0900

    first commit

--- App.js
+++ App.js
@@ -1,49 +1,8 @@
-import * as React from 'react';
-import { View, Text, Button, Image, ImageBackground } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react'
+import AuthNavigator from './src/navigation/AuthNavigator'
 
-function HomeScreen() {
-  return (
-    <View style={{flex: 1}}>
-    <Text>Hello</Text>
-    </View>
-  );
+const App = () => {
+  return <AuthNavigator />
 }
 
-function LogoTitle() {
-  return (
-  <Image  source={require('./image/worksystem.png')} style={{height:50, width:200,resizeMode:'contain'}} /> 
-  ); 
-}
-
-function HeaderR() {
-  return ( <Image  source={require('./image/search.png')} style={{height:20, width:30,resizeMode:'contain'}} /> );
- 
-}
-function HeaderL(){
-  return(<Image source={require('./image/menu.png')} style={{height:20, width: 30,justifyContent:'center', alignItems:'center',  resizeMode:'contain'}}/> );
-  
-}
-
-const Stack = createStackNavigator();
- 
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" options={{
-          headerStyle: { backgroundColor: '#101152'},
-          headerTitle: props => <LogoTitle {...props}/>,
-          headerTitleAlign: 'center',
-          headerRight: ()=> <HeaderR/>,
-          headerLeftContainerStyle: {paddingLeft: 10},
-          headerRightContainerStyle: {paddingRight: 10},
-          headerLeft: ()=> <HeaderL />
-        }} component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
-
-export default App;
+export default App
